9c71746017258295765b593cd671b2395baf5f3e
Ethan Greenwald <etgreen13@gmail.com>
2022-11-08 13:00:01 -0600

    Fix makefile and add .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 using namespace std;
-
+//to build and run, go to /build and run `make && ./main`
 int main(){
     Graph* graph = new Graph();
     vector<WikiNode *> links = graph->getPage("%C3%85land")->getLinks();
